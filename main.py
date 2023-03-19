3cc3ca7b22727b871903cfb1c7c51eb89502a555
Adam Nagy <adamndracon@gmail.com>
2023-03-19 20:35:14 +0000

    amazon_price_checker

--- main.py
+++ main.py
@@ -7,32 +7,43 @@ import time
 import requests
 import schedule
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 def main():
     url = "https://www.amazon.co.uk/Blue-Recording-Streaming-Condenser-Adjustable/dp/B00N1YPXW2/ref=sr_1_125?content-id=amzn1.sym.222608eb-f887-4329-bb45-ef37761e5b56&pd_rd_r=d67a9db9-a376-46d1-9369-5e90693895aa&pd_rd_w=gMU8i&pd_rd_wg=01rrq&pf_rd_p=222608eb-f887-4329-bb45-ef37761e5b56&pf_rd_r=P2QBE3C7EPCN245E6YXQ&refinements=p_72%3A405434031%2Cp_36%3A-20000&rnid=405443031&s=musical-instruments&sr=1-125"
 
+    from selenium import webdriver
+
     def get_price(url):
-        HEADERS = {
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
-            "Accept-Language": "en-US, en;q=0.5",
-        }
-        response = requests.get(
-            url,
-            headers=HEADERS,
-        )
-        soup = BeautifulSoup(
-            response.content,
-            "html.parser",
-        )
-        with open("test.html", "w", encoding="utf-8") as file:
-            file.write(str(soup))
+        driver = webdriver.Chrome("../../_Tools/webdriver/chromedriver.exe")
+        driver.get(url)
+        price = int(driver.find_element(By.CSS_SELECTOR, "span.a-price-whole").text)
+        name = " ".join(driver.find_element(By.ID, "productTitle").text.split(" ")[:3])
+        check_deal(price, name, 60)
 
-        item_price = float(soup.find("span", "a-offscreen").text[1:])
-        # title = soup.find("h1", id="title")
-        title = " ".join(soup.find("h1", id="title").text.strip().split(" ")[:3])
+    # def get_price(url):
+    #     HEADERS = {
+    #         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+    #         "Accept-Language": "en-US, en;q=0.5",
+    #     }
+    #     response = requests.get(
+    #         url,
+    #         headers=HEADERS,
+    #     )
+    #     soup = BeautifulSoup(
+    #         response.content,
+    #         "html.parser",
+    #     )
+    #     with open("test.html", "w", encoding="utf-8") as file:
+    #         file.write(str(soup))
 
-        check_deal(item_price, title, 60)
+    #     item_price = float(soup.find("span", "a-offscreen").text[1:])
+    #     # title = soup.find("h1", id="title")
+    #     title = " ".join(soup.find("h1", id="title").text.strip().split(" ")[:3])
+
+    #     check_deal(item_price, title, 60)
 
     def tracking(get_price):
         get_price(url)
